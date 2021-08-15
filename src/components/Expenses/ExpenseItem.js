99fbf964633845784de7584bebeac52d435b0858
FunkyFeudalFox <arjan_tammer@yahoo.co.uk>
2021-08-15 21:12:10 +0200

    added NewExpense and ExpenseForm

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,12 +1,15 @@
+import React, { useState } from "react";
+
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
-import Card from "./Card.js";
+import Card from "../UI/Card.js";
 
 function ExpenseItem(props) {
-  let title = props.title;
+  const [title, setTitle] = useState(props.title);
 
   const clickHandler = () => {
-    title = "Updated!";
+    setTitle("Updated!");
+    console.log(title);
   };
 
   return (
